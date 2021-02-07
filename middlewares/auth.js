c2a3602f22cd69c7b64a44add79ac10dac58d57f
fiixed <fiixed@gmail.com>
2021-02-07 16:14:32 -0500

    admin check

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin = require('../firebase/index');
+const User = require('../models/user');
 
 exports.authCheck = async (req, res, next) => {
     //console.log(req.headers); //token
@@ -17,3 +18,17 @@ exports.authCheck = async (req, res, next) => {
     }
     
 };
+
+exports.adminCheck = async (req, res, next) => {
+    const { email } = req.user;
+
+    const adminUser = await User.findOne({email}).exec();
+
+    if (adminUser.role !== 'admin') {
+        res.staus(403).json({
+            err: 'Admin resource.  Access denied',
+        })
+    } else {
+        next();
+    }
+}

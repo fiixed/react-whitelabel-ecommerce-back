36f220e0aff5da7f6cc3c4e11c60c0ebec7bb587
fiixed <fiixed@gmail.com>
2021-02-08 16:45:34 -0500

    finished category controllers

--- controllers/category.js
+++ controllers/category.js
@@ -13,18 +13,32 @@ exports.create = async (req, res) => {
     }
 };
 
-exports.list = async (req, res) => {
+exports.list = async (req, res) => 
+    res.json(await Category.find({}).sort({createdAt: -1}).exec());
 
-};
 
 exports.read = async (req, res) => {
-
+    let category = await Category.findOne({slug: req.params.slug}).exec();
+    res.json(category);
 };
 
 exports.update = async (req, res) => {
-
+    const { name } = req.body
+    try {
+        const updated = await Category.findOneAndUpdate({slug: req.params.slug}, {name, slug: slugify(name)}, { new: true}).exec();
+        res.json(updated);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send('Update category failed');
+    }
 };
 
 exports.remove = async (req, res) => {
-
+    try {
+        const deleted = await Category.findOneAndDelete({slug: req.params.slug}).exec();
+        res.json(deleted);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send('Delete category failed');
+    }
 };

b25805659c7ca4ab49fe5d7fbd5a49108bbdcea1
fiixed <fiixed@gmail.com>
2021-02-11 15:55:17 -0500

    add parent to update sub controller

--- controllers/sub.js
+++ controllers/sub.js
@@ -23,9 +23,12 @@ exports.read = async (req, res) => {
 };
 
 exports.update = async (req, res) => {
-    const { name } = req.body
+    const { name, parent } = req.body
     try {
-        const updated = await Sub.findOneAndUpdate({slug: req.params.slug}, {name, slug: slugify(name)}, { new: true}).exec();
+        const updated = await Sub.findOneAndUpdate(
+            {slug: req.params.slug}, 
+            {name, parent, slug: slugify(name)}, 
+            { new: true}).exec();
         res.json(updated);
     } catch (err) {
         console.log(err);

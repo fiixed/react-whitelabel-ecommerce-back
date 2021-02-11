be2f5eb834be8e8d048297b019d35e252ae37431
fiixed <fiixed@gmail.com>
2021-02-11 17:34:11 -0500

    product create route and controller

---
+++ routes/product.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+// middleware
+const { authCheck, adminCheck } = require('../middlewares/auth');
+
+// controller
+const { create }  = require('../controllers/product');
+
+// routes
+router.post('/product', authCheck, adminCheck, create);
+
+
+module.exports = router;

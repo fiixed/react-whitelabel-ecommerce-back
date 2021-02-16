438aec8aad12a2319394609d80be8eb58a1a6665
fiixed <fiixed@gmail.com>
2021-02-16 10:46:17 -0500

    products total count endpoint

--- controllers/product.js
+++ controllers/product.js
@@ -79,3 +79,8 @@ exports.list = async (req, res) => {
     }
 }
 
+exports.productsCount = async (req, res) => {
+    let total = await Product.find({}).estimatedDocumentCount().exec();
+    res.json(total);
+}
+

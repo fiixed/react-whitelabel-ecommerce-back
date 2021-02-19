b71ef500d05b6cf5644d4bafbf5d49f2b3b1fc56
fiixed <fiixed@gmail.com>
2021-02-19 15:55:31 -0500

    sub read products endpoint

--- controllers/category.js
+++ controllers/category.js
@@ -20,15 +20,15 @@ exports.list = async (req, res) =>
 
 
 exports.read = async (req, res) => {
-        let category = await Category.findOne({ slug: req.params.slug }).exec();
-        // res.json(category);
-        const products = await Product.find({ category }).populate("category").exec();
+    let category = await Category.findOne({ slug: req.params.slug }).exec();
+    // res.json(category);
+    const products = await Product.find({ category }).populate("category").exec();
       
-        res.json({
-          category,
-          products,
-        });
-      };
+    res.json({
+        category,
+        products,
+    });
+};
 
 exports.update = async (req, res) => {
     const { name } = req.body
